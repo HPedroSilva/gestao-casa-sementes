0adda05f0ce793c025d8b761bd132910353b716f
Pedro Henrique <pedrohs21@outlook.com>
2022-11-02 13:47:14 -0300

    Implementação da seleção dos recipientes no registro de saída.

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -3,6 +3,7 @@ from mainApp.models import *
 from dynamic_admin_forms.admin import DynamicModelAdminMixin
 from django.shortcuts import redirect
 from django.urls import reverse
+from django import forms
 class EnderecoInline(admin.StackedInline):
     model = Endereco
     max_num = 3
@@ -42,6 +43,23 @@ class RegistroEntradaAdmin(DynamicModelAdminMixin, admin.ModelAdmin):
         for recipiente in recipientes:
             recipiente.gerarQrCode(request.build_absolute_uri(reverse('mainApp:recipiente', args=[recipiente.id])))
         return redirect(reverse('mainApp:etiquetas_registro_entrada', args=[obj.id]))
+class RegistroSaidaForm(forms.ModelForm):
+    recipientes = forms.ModelMultipleChoiceField(Recipiente.objects.filter(registroSaida=None))
+    class Meta:
+        model = RegistroSaida
+        fields = ["data", "descricao", "destino"]
+@admin.register(RegistroSaida)
+class RegistroSaidaAdmin(admin.ModelAdmin):
+    form = RegistroSaidaForm
+
+    def save_model(self, request, obj, form, change):
+        if form.is_valid():
+            registroSaida = form.save()
+            recipientes = form.cleaned_data['recipientes']
+            for recipiente in recipientes:
+                recipiente.registroSaida = registroSaida
+                recipiente.save()
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Especie)
 admin.site.register(Variedade)
@@ -49,10 +67,6 @@ admin.site.register(Armario)
 admin.site.register(Sensor)
 admin.site.register(Posicao)
 admin.site.register(Unidade)
-admin.site.register(RegistroSaida)
 admin.site.register(TipoTeste)
 admin.site.register(Teste)
 admin.site.register(Recipiente)
-
-
-
